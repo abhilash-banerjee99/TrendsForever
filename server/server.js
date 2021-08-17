e88e94890f42af6e2e17c7c41025039faeb78f5f
abhilash-banerjee99 <digiabhi1999@gmail.com>
2021-08-18 00:00:59 +0530

    Add few changes in server
    Add few changes in server

--- server/server.js
+++ server/server.js
@@ -1,21 +1,46 @@
 const express = require('express');
 const cors = require('cors');
-//const bodyParser = require('body-parser');
-const dotenv = require('dotenv');
+const path = require('path');
 
-dotenv.config();
+if(process.env.NODE_ENV !== 'production') require('dotenv').config();
+
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 const app = express();
+const port = process.env.PORT || 5000;
 
-// app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 app.use(cors());
 
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')))
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
 const configureRoutes = require('./routes');
 
 configureRoutes(app);
 
-app.listen(8000, error => {
+app.listen(port, error => {
   if (error) throw error;
-  console.log('Server running on port ' + 8000);
+  console.log('Server running on port ' + port);
+});
+
+app.post('/payment', (req, res) => {
+  const body = {
+    source: req.body.token.id,
+    amount: req.body.amount,
+    currency: 'usd'
+  };
+
+  stripe.charges.create(body, (stripeErr, stripeRes) => {
+    if (stripeErr) {
+      res.status(500).send({ error: stripeErr });
+    } else {
+      res.status(200).send({ success: stripeRes });
+    }
+  });
 });
